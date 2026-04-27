87d30f548ba7c794b57e8f064502ff8799a2f8bc
Atefeh-Amjadian <am.atefeh1370@gmail.com>
2026-04-27 18:46:41 +0300

    Implement symptom feature extraction

--- src/features.py
+++ src/features.py
@@ -2,8 +2,51 @@ import pandas as pd
 
 
 def extract_symptoms(df_raw):
-    # TODO: extract symptoms
-    return None
+    """
+    Extract Present Illness symptoms as binary features.
+    """
+
+    symptom_cols = ['Unnamed: 6'] + list(df_raw.columns[7:36])
+    df_symptoms_raw = df_raw[symptom_cols].copy()
+
+    # Remove form header rows
+    df_symptoms = df_symptoms_raw.iloc[3:].reset_index(drop=True)
+
+    symptom_rename_map = {
+        'Unnamed: 8': 'Fever',
+        'Unnamed: 9': 'Myalgia',
+        'Unnamed: 10': 'Chills',
+        'Unnamed: 11': 'Fatigue',
+        'Unnamed: 12': 'Sweating',
+        'Unnamed: 13': 'Anorexia',
+        'Unnamed: 14': 'Sore_Throat',
+        'Unnamed: 15': 'Sneezing',
+        'Unnamed: 16': 'Cough',
+        'Unnamed: 17': 'Dyspnea',
+        'Unnamed: 18': 'Eye_Burning',
+        'Unnamed: 19': 'Anosmia',
+        'Unnamed: 20': 'Rhinorrhea',
+        'Unnamed: 21': 'Voice_Change',
+        'Unnamed: 22': 'Nasal_Congestion',
+        'Unnamed: 23': 'Ageusia',
+        'Unnamed: 24': 'Chest_Pain',
+        'Unnamed: 25': 'Palpitations',
+        'Unnamed: 26': 'Nausea',
+        'Unnamed: 27': 'Vomiting',
+        'Unnamed: 28': 'Abdominal_Pain',
+        'Unnamed: 29': 'Diarrhea',
+        'Unnamed: 32': 'Depression',
+        'Unnamed: 33': 'Sleep_Disorder',
+        'Unnamed: 34': 'Headache',
+    }
+
+    df_symptoms_binary = df_symptoms.notna().astype(int)
+    df_symptoms_binary.rename(columns=symptom_rename_map, inplace=True)
+
+    symptom_features = list(symptom_rename_map.values())
+    df_symptoms_final = df_symptoms_binary[symptom_features].copy()
+
+    return df_symptoms_final
 
 
 def extract_pmh(df_raw):
